c9dae108e4f44c5084112a900d9ba87a38066386
reverseAndroid <m13186158003@163.com>
2020-05-07 10:22:26 +0800

    修改activityList到APP里

--- app/src/main/java/com/xjx/mvp/App.kt
+++ app/src/main/java/com/xjx/mvp/App.kt
@@ -1,12 +1,16 @@
 package com.xjx.mvp
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import java.util.*
 
 class App : Application() {
 
     companion object {
         private var mContext: Context? = null
+
+        var mActivityList = ArrayList<Activity>()
 
         fun getContext(): Context? {
             return mContext
--- app/src/main/java/com/xjx/mvp/base/BaseActivity.kt
+++ app/src/main/java/com/xjx/mvp/base/BaseActivity.kt
@@ -3,19 +3,19 @@ package com.xjx.mvp.base
 import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xjx.mvp.App
 import com.xjx.mvp.utils.Retrofit.ApiRetrofit
 
 abstract class BaseActivity<A, V, P : BasePresenter<A, V>> : AppCompatActivity(), BaseViewImp<A> {
 
     var mPresenter: P? = null
     var mActivity: A? = null
-    var mActivityList = ArrayList<Activity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(initContentView())
         mActivity = getCurrentActivity()
-        mActivityList.add(mActivity as Activity)
+        App.mActivityList.add(mActivity as Activity)
         mPresenter = initPresenter()
         mPresenter?.attachView(mActivity as A, this as V)
     }
@@ -26,7 +26,7 @@ abstract class BaseActivity<A, V, P : BasePresenter<A, V>> : AppCompatActivity()
 
     override fun onDestroy() {
         super.onDestroy()
-        mActivityList.remove(mActivity as Activity)
+        App.mActivityList.remove(mActivity as Activity)
         mPresenter?.detachView()
         mPresenter = null
     }
